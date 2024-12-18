f8df25fe67fc2afea8bc3006766c89c69b9b7258
0ver1ay <107620656+0ver1ay@users.noreply.github.com>
2024-12-18 23:32:59 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 def save_tables(url, class_name):
-    # Fetch the HTML content
+   
     response = requests.get(url)
     html_content = response.text
 
-    # Parse the HTML using BeautifulSoup
+   
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Find all tables with the specified class name
+    
     tables = soup.find_all('table', class_=class_name)
 
     for i, table in enumerate(tables):
-        # Save each table in a file with a unique name
+        
         output_file = f'html_input_{i+1}.txt'
         with open(output_file, 'w', encoding='utf-8') as file:
             # Extract table content without opening and closing tags
@@ -22,7 +22,7 @@ def save_tables(url, class_name):
             file.write('\n')
         print(f"Table {i+1} saved in {output_file}.")
 
-# Example usage
+
 url = input("Enter URL\n")
 class_name = 'specif'
 
